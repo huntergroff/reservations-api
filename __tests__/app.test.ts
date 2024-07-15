5831179165b6a27d1cf3a22d6155f8e47e5bac2a
Hunter Groff <huntergroff22@gmail.com>
2024-07-15 19:01:17 -0400

    create src directory

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -1,6 +1,6 @@
 import request from "supertest";
-import app from "../app";
-import basetext from "../app";
+import app from "../src/app";
+import basetext from "../src/app";
 
 describe("GET /", () => {
   it("should return 200 OK", async () => {
--- __tests__/reservations.test.ts
+++ __tests__/reservations.test.ts
@@ -1,5 +1,5 @@
 import request from "supertest";
-import app from "../app";
+import app from "../src/app";
 
 // Define a reservation object for testing which is known to exist in the database
 const reservation1 = {
